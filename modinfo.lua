72531f472671e88fe00f6d9f93f6aad15da5c949
splorange12 <trentw25@frontier.com>
2023-10-20 01:42:27 -0400

    github release

--- modinfo.lua
+++ modinfo.lua
@@ -1,10 +1,10 @@
-name = "tentacle detection"
+name = "Tentacle Detection"
 --The name of your mod
-description = "grants vision to nearby tentacles"
+description = "Grants vision to nearby tentacles"
 --The description that shows when you are selecting the mod from the list
 author = "splorange"
 --Your name!
-version = "1.2"
+version = "1.0"
 
 forumthread = ""
 
@@ -26,26 +26,3 @@ client_only_mod = true
 
 api_version = 10
 --This is the version of the game's API and should stay the same for the most part
-configuration_options =
-{
-    {
-        name = "scale",
-        label = "Willow Size",
-        hover = "Change the size Willow will be in game!",
-        options =
-        {
-            {description = "100% (No Change)", data = 1.0},
-            {description = "90% (Slightly Smaller)", data = 0.9},
-            {description = "80% (Definitely Smaller)", data = 0.8},
-            {description = "70% (Babu)", data = 0.7},
-            {description = "60% (Pinchable cheeks)", data = 0.6},
-            {description = "50% (Carry-on size)", data = 0.5},
-            {description = "40% (Super baby)", data = 0.4},
-            {description = "30% (Actual baby)", data = 0.3},
-            {description = "20% (Punchkin)", data = 0.2},
-            {description = "10% (Where'd she go?)", data = 0.1},
-            {description = "5% (Shrink ray)", data = 0.05},
-        },
-        default = "0.9",
-    },
-}
--- modmain.lua
+++ modmain.lua
@@ -1,26 +1,9 @@
-local _G = GLOBAL
-local FollowText = require "widgets/followtext"
-local Image = require "widgets/image"
-local require = GLOBAL.require
-local Vector3 = GLOBAL.Vector3
-local DEFAULT_OFFSET = Vector3(0, -400, 0)
-
-if _G.TheNet:IsDedicated() then return end
-
-if _G.TheNet:GetServerGameMode() == "lavaarena" then return end
-
-
---[[Assets = {
-    Asset("ATLAS", "images/wgt_arrow.xml"),
-    Asset("IMAGE","images/wgt_arrow.tex"),
-}]]--
-
 Assets = {
 	Asset("ANIM", "images/trap_marker_fx.zip")
 }
 
 local function MakeSkull(inst, scale)
-	local skull = _G.CreateEntity()
+	local skull = GLOBAL.CreateEntity()
 
 	skull.entity:SetCanSleep(false)
 	skull.persists = false
@@ -39,27 +22,10 @@ local function MakeSkull(inst, scale)
     skull.AnimState:SetTime(math.random()*3)
 
 	skull.AnimState:SetBloomEffectHandle( "shaders/anim.ksh" )  
-	--[[circle.AnimState:SetOrientation(_G.ANIM_ORIENTATION.OnGround)
-	circle.AnimState:SetLayer(_G.LAYER_BACKGROUND)
-	circle.AnimState:SetSortOrder(3.1)]]--
 
 	skull.entity:SetParent(inst.entity)
 	return skull
 end
-
-
---[[local function AddArrowToTentacle(inst)
-	if _G.ThePlayer ~= nil and _G.ThePlayer.HUD ~= nil then
-	inst.playerhud = _G.ThePlayer.HUD:AddChild(FollowText(_G.TALKINGFONT, 35))
-	inst.playerhud:SetHUD(_G.ThePlayer.HUD.inst)
-    local offset = Vector3(0,-220,0)
-    inst.playerhud:SetOffset(DEFAULT_OFFSET-offset)
-    inst.playerhud:SetTarget(inst)
-	inst.playerhud.arrow = inst.playerhud:AddChild(Image("images/wgt_arrow.xml", "wgt_arrow.tex"))
-    inst.playerhud.arrow:SetScale(.25,.25,1)
-
-	end
-end]]--
 
 AddPrefabPostInit("tentacle", function(inst)
   --AddArrowToTentacle(inst)
